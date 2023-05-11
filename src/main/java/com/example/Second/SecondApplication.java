813cfc015c9bef531921f19a5e64a91f6c102a5c
test <test@testui-Mac-mini.local>
2023-05-12 00:02:23 +0900

    corr hello2

--- src/main/java/com/example/Second/SecondApplication.java
+++ src/main/java/com/example/Second/SecondApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecondApplication {
 
 	public static void main(String[] args) {
-		Sysout.out.println("hello");
+		System.out.println("hello");
 		SpringApplication.run(SecondApplication.class, args);
 	}
 
